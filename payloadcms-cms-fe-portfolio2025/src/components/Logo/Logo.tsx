ef9f075c073a88c4caa560a68a899281b0e88cd5
Patrick Meaney <patrick.wm.meaney@gmail.com>
2025-03-31 17:38:04 -0600

    Dockerfile fix

--- payloadcms-cms-fe-portfolio2025/src/components/Logo/Logo.tsx
+++ payloadcms-cms-fe-portfolio2025/src/components/Logo/Logo.tsx
@@ -6,7 +6,7 @@ interface Props {
   loading?: 'lazy' | 'eager'
   priority?: 'auto' | 'high' | 'low'
 }
-// Test123456
+// Test
 export const Logo = (props: Props) => {
   const { loading: loadingFromProps, priority: priorityFromProps, className } = props
 
