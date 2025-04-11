00c0150168f1c7972c18791523a25e2ce42bfa38
Patrick Meaney <patrick.wm.meaney@gmail.com>
2025-04-11 14:38:32 -0600

    removed migrations files

--- payloadcms-cms-fe-portfolio2025/src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250402_002156 from './20250402_002156';
-
-export const migrations = [
-  {
-    up: migration_20250402_002156.up,
-    down: migration_20250402_002156.down,
-    name: '20250402_002156'
-  },
-];
