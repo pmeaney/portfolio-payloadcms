9cd6b0233a82c3851c7f011a427ad4145f4d3dd8
Patrick Meaney <patrick.wm.meaney@gmail.com>
2025-04-13 12:43:27 -0600

    test deploy-- forgot to delete the migration files previously

--- payloadcms-cms-fe-portfolio2025/src/heros/HighImpact/index.tsx
+++ payloadcms-cms-fe-portfolio2025/src/heros/HighImpact/index.tsx
@@ -8,7 +8,7 @@ import { CMSLink } from '@/components/Link'
 import { Media } from '@/components/Media'
 import RichText from '@/components/RichText'
 
-// testing
+// testing testing
 
 export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText }) => {
   const { setHeaderTheme } = useHeaderTheme()
--- payloadcms-cms-fe-portfolio2025/src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20250412_194416 from './20250412_194416';
-
-export const migrations = [
-  {
-    up: migration_20250412_194416.up,
-    down: migration_20250412_194416.down,
-    name: '20250412_194416'
-  },
-];
