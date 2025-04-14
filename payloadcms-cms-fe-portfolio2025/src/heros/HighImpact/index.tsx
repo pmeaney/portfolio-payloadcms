45bcc6f3b6a453a9c832152d0be50d692c90d2e4
Patrick Meaney <patrick.wm.meaney@gmail.com>
2025-04-14 10:47:42 -0600

    Updated logo component.  Fixed CICD where it was getting confused about prior commit obbjects commit hashes getting changed

--- payloadcms-cms-fe-portfolio2025/src/heros/HighImpact/index.tsx
+++ payloadcms-cms-fe-portfolio2025/src/heros/HighImpact/index.tsx
@@ -23,6 +23,7 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
       data-theme="dark"
     >
       <div className="container mb-8 z-10 relative flex items-center justify-center">
+      {/* <div className="max-w-[40rem] md:text-left"> */}
         <div className="max-w-[36.5rem] md:text-center">
           {richText && <RichText className="mb-6" data={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
